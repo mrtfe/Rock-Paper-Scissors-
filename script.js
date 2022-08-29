7fd9f0154b6c9e68c5b17d6439c5b0f20c903762
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-08-29 21:24:55 +0300

    play round function halfway

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("hello, your javascript file is loaded");
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+const computerSelection = getComputerChoice();
+console.log(`Computer choice: ${getComputerChoice()}`);
+
+const playerSelection = "paper";
+console.log(`Player choice: ${playerSelection}`);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats scissors";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beat paper";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats rock";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beat paper";
+  }
+}
+
+// console.log(playRound(playerSelection, computerSelection));
