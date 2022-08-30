843f319a6345ebbcd40c596fecc279fbef7d58ba
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-08-30 20:17:33 +0300

    mapped functions with HTML

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
+const ROCK = document.querySelector(".rock");
+const PAPER = document.querySelector(".paper");
+const SCISSORS = document.querySelector(".scissors");
+const playerLiveResult = document.querySelector(".player-live-result");
+const computerLiveResult = document.querySelector(".computer-live-result");
+const gameLog = document.querySelector(".game-log__container");
+
+ROCK.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+  playerLiveResult.innerHTML = playerPoints;
+  computerLiveResult.innerHTML = computerPoints;
+});
+PAPER.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+  playerLiveResult.innerHTML = playerPoints;
+  computerLiveResult.innerHTML = computerPoints;
+});
+SCISSORS.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+  playerLiveResult.innerHTML = playerPoints;
+  computerLiveResult.innerHTML = computerPoints;
+});
+
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
+
 let playerPoints = 0;
 let computerPoints = 0;
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "Tie";
+    return (gameLog.innerHTML = "Tie");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerPoints++;
-    return "You Lose! Paper beats Rock";
+    return (gameLog.innerHTML = "You Lose! Paper beats Rock");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerPoints++;
-    return "You Win! Rock beats scissors";
+    return (gameLog.innerHTML = "You Win! Rock beats scissors");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerPoints++;
-    return "You Lose! Scissors beat paper";
+    return (gameLog.innerHTML = "You Lose! Scissors beat paper");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerPoints++;
-    return "You Win! Paper beats rock";
+    return (gameLog.innerHTML = "You Win! Paper beats rock");
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerPoints++;
-    return "You Lose! Rock beats scissors";
+    return (gameLog.innerHTML = "You Lose! Rock beats scissors");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerPoints++;
-    return "You Win! Scissors beat paper";
+    return (gameLog.innerHTML = "You Win! Scissors beat paper");
   }
 }
 
@@ -39,13 +64,13 @@ function game() {
   }
 }
 
-const computerSelection = getComputerChoice();
-console.log(`Computer choice: ${computerSelection}`);
+// const computerSelection = getComputerChoice();
+// console.log(`Computer choice: ${computerSelection}`);
 
-const playerSelection = "paper";
-console.log(`Player choice: ${playerSelection}`);
+// const playerSelection = "paper";
+// console.log(`Player choice: ${playerSelection}`);
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
-console.log(`Player points: ${playerPoints}`);
-console.log(`Computer points: ${computerPoints}`);
+// console.log(`Player points: ${playerPoints}`);
+// console.log(`Computer points: ${computerPoints}`);
